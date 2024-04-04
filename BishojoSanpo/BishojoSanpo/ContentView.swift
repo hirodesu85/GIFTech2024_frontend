8e60fc0e62b51c9fb69d55cb596004547d04e78f
madomaru <melondoka1502@gmail.com>
2024-04-04 16:12:18 +0900

    create base code

---
+++ BishojoSanpo/BishojoSanpo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BishojoSanpo
+//
+//  Created by 伊藤まどか on 2024/04/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
