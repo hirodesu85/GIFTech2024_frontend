73271868b3dd2851dba4c54d7c27241bb48ffe91
OGATA Hiromu <hiromukkun@gmail.com>
2024-04-25 23:55:42 +0900

    タブの切替ごとに画像変更

--- BishojoSanpo/BishojoSanpo/Features/DressUp/ItemTable.swift
+++ BishojoSanpo/BishojoSanpo/Features/DressUp/ItemTable.swift
@@ -8,29 +8,78 @@
 import SwiftUI
 
 struct ItemTable: View {
-    @StateObject private var itemListModel = ItemListModel()
+    @ObservedObject var itemListModel: ItemListModel
+    @Binding var selectedCategory: Int
     let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 2)
+    
     
     var body: some View {
         VStack(spacing: 0) {
             if let catalog = itemListModel.catalog {
-                HStack(spacing:0) {
-                    WebPImageView(imageName: "TagNormalHair.webp")
-                    WebPImageView(imageName: "TagNormalHair.webp")
-                    WebPImageView(imageName: "TagNormalHair.webp")
-                    WebPImageView(imageName: "TagNormalHair.webp")
+                HStack(alignment: .bottom, spacing: 0) {
+                    Button(action: {
+                        selectedCategory = 0
+                    }) {
+                        if selectedCategory == 0 {
+                            WebPImageView(imageName: "TagActiveHair.webp")
+                                .frame(width: 50)
+                        } else {
+                            WebPImageView(imageName: "TagNormalHair.webp")
+                                .frame(width: 50)
+                        }
+                    }
+                    
+                    Button(action: {
+                        selectedCategory = 1
+                    }) {
+                        if selectedCategory == 1 {
+                            WebPImageView(imageName: "TagActiveOver.webp")
+                                .frame(width: 50)
+                        } else {
+                            WebPImageView(imageName: "TagNormalOver.webp")
+                                .frame(width: 50)
+                        }
+                    }
+                    
+                    Button(action: {
+                        selectedCategory = 2
+                    }) {
+                        if selectedCategory == 2 {
+                            WebPImageView(imageName: "TagActiveUnder.webp")
+                                .frame(width: 50)
+                        } else {
+                            WebPImageView(imageName: "TagNormalUnder.webp")
+                                .frame(width: 50)
+                        }
+                    }
+                    
+                    Button(action: {
+                        selectedCategory = 3
+                    }) {
+                        if selectedCategory == 3 {
+                            WebPImageView(imageName: "TagActiveShoes.webp")
+                                .frame(width: 50)
+                        } else {
+                            WebPImageView(imageName: "TagNormalShoes.webp")
+                                .frame(width: 50)
+                        }
+                    }
+                    
                 }
+                
                 ZStack {
-                    WebPImageView(imageName: "BackWhite.webp")
-                        .frame(width: 377)
-                        .border(/*@START_MENU_TOKEN@*/Color.black/*@END_MENU_TOKEN@*/)
+                    WebPImageView(imageName: "NewBackWhite.webp")
+                        .scaledToFill()
+                        .frame(width: 200, height: 450)
+                        .clipped()
+                        .blur(radius: 3.0)
                     ScrollView {
-                        LazyVGrid(columns: columns, spacing: 20) {
-                            ForEach(catalog.hairs, id: \.self.id) { hair in
+                        LazyVGrid(columns: columns, spacing: 5) {
+                            ForEach(itemsForSelectedCategory(catalog: catalog), id: \.self.id) { item in
                                 ZStack {
                                     WebPImageView(imageName: "ItemBlank.webp")
-                                        .frame(width: 170, height: 170)
-                                    AsyncImage(url: URL(string: hair.imageUrl)) { phase in
+                                        .frame(width: 100, height: 100)
+                                    AsyncImage(url: URL(string: item.imageUrl)) { phase in
                                         if let image = phase.image {
                                             image.resizable()
                                         } else if phase.error != nil {
@@ -38,16 +87,18 @@ struct ItemTable: View {
                                         } else {
                                             Text("画像取得中...")
                                         }
-                                    }.frame(width: 100, height: 100)
+                                    }.frame(width: 55, height: 55)
+                                        .offset(x: -3, y: 3)
                                 }
                             }
                             
-                            ForEach(0..<12-catalog.hairs.count, id: \.self) { _ in
+                            ForEach(0..<12-itemsForSelectedCategory(catalog: catalog).count, id: \.self) { _ in
                                 WebPImageView(imageName: "ItemLock.webp")
-                                    .frame(width: 170, height: 170)
+                                    .frame(width: 100, height: 100)
                             }
                         }
                     }
+                    .frame(width: 200, height: 450)
                 }
             } else {
                 Text("データをロード中...")
@@ -58,8 +109,23 @@ struct ItemTable: View {
             }
         }
     }
+    
+    func itemsForSelectedCategory(catalog: Catalog) -> [Item] {
+        switch selectedCategory {
+        case 0:
+            return catalog.hairs
+        case 1:
+            return catalog.tops
+        case 2:
+            return catalog.bottoms
+        case 3:
+            return catalog.shoes
+        default:
+            return []
+        }
+    }
 }
 
 #Preview {
-    ItemTable()
+    ItemListView()
 }
