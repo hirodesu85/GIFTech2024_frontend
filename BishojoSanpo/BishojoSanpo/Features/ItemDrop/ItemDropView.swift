562d6ac85ed13adb94d7ed11f4f1aafa22ccda0b
吉野敬太郎 <yoshinoyukitarou@yoshinokeitarounoMacBook-Pro.local>
2024-04-25 23:14:25 +0900

    feat: アイテム獲得画面でタップしてから画像を表示するところまで実装した

--- BishojoSanpo/BishojoSanpo/Features/ItemDrop/ItemDropView.swift
+++ BishojoSanpo/BishojoSanpo/Features/ItemDrop/ItemDropView.swift
@@ -15,7 +15,14 @@ struct ItemDropView: View {
     
     var body: some View {
         ZStack{
+            //背景が動くならここを書き換える
+            WebPImageView(imageName: "Background.webp")
+                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                .edgesIgnoringSafeArea(.all)
             VStack{
+                WebPImageView(imageName: "present_box.webp").aspectRatio(contentMode: .fit).frame(width: UIScreen.main.bounds.width * 0.8)
+                WebPImageView(imageName: "tap_to_open.webp").aspectRatio(contentMode: .fit).frame(width: UIScreen.main.bounds.width * 0.7)
+                /*
                 Text("プレゼントの画像を表示")
                 Text("Tap Open!")
                 if let dropItem = rewardModel.dropItem{
@@ -23,11 +30,40 @@ struct ItemDropView: View {
                     Text(dropItem.name)
                 }else{
                     Text("アイテムをロード中")
-                }
+                }*/
             }
             DetectTapView(isTapped: $isTapped)
+            VStack {
+                Button(action: {
+                    router.returnToHome()
+                }) {
+                    WebPImageView(imageName: "skip_button.webp").aspectRatio(contentMode: .fit)
+                        .frame(width: 70, height: 70)
+                        .padding(.top, 15)
+                        .padding(.trailing, 12)
+                }
+            }.frame(
+                width: UIScreen.main.bounds.width,
+                height: UIScreen.main.bounds.height,
+                alignment: 
+                    .topTrailing
+            )
                 
-            HomeButtonView()
+            if let dropItem = rewardModel.dropItem , isTapped {
+                AsyncImage(url: URL(string: dropItem.imageUrl)) { phase in
+                    if let dropItemImage = phase.image {
+                        ZStack {
+                            WebPImageView(imageName: "effect.webp")
+                            dropItemImage.resizable()
+                                .aspectRatio(contentMode: .fit)
+                        }
+                    } else if phase.error != nil {
+                        Text("画像読み込みエラー")
+                    } else {
+                        Text("ロード中")
+                    }
+                }
+            }
                 
         }
         .ignoresSafeArea()
