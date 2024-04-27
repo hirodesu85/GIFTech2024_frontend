eeb93198ab01dc324181740538ce72c8a281f9cd
madomaru <melondoka1502@gmail.com>
2024-04-28 03:25:08 +0900

    マップ画面と好感度ダイアログにボタンアニメーションを追加

--- BishojoSanpo/BishojoSanpo/Features/Map/ArrivedButtonView.swift
+++ BishojoSanpo/BishojoSanpo/Features/Map/ArrivedButtonView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ArrivedButtonView: View {
     @EnvironmentObject var router: NavigationRouter
+    @Binding var isPressed: Bool
     let goalData: GoalData
     var body: some View {
         ZStack {
@@ -17,24 +18,28 @@ struct ArrivedButtonView: View {
                 VStack {
                     Spacer()
                     Button(action: {
-                        router.navigateToItemDrop(with: goalData)
-                        print("a")
+                        
                         AudioPlayer.shared.playSound()
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                            router.navigateToItemDrop(with: goalData)
+                        }
                     }) {
                         WebPImageView(imageName: "ArrivedButton.webp")
+                            .scaleEffect(isPressed ? 1.2 : 1)
+                            .animation(.easeInOut(duration: 0.2), value: isPressed)
                             .frame(width: 150, height: 100) // ここで高さも指定
                             .padding(.bottom, 18)
                             .padding(.leading, 18)
                     }
+                    .simultaneousGesture(
+                        DragGesture(minimumDistance: 0)
+                            .onChanged { _ in isPressed = true }
+                            .onEnded { _ in isPressed = false }
+                    )
+                    
                 }
                 Spacer()
             }
         }
     }
-}
-
-
-#Preview {
-    let goalData = GoalData()
-    return ArrivedButtonView(goalData: goalData)
 }
